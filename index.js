0c6a24ff8c60b672642a7d3728bd0f15d34ea0bd
phuoctv1303 <phuoctv@maxmobile-software.com>
2022-08-16 10:34:36 +0700

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const { Client } = require('discord.js');
 const prism = require('prism-media');
 const config = require('./config.json');
 const ytdl = require('ytdl-core');
+const express = require('express');
+const port = 80;
+const app = express();
+app.get('/', function (req, res) {
+	res.send('Hello World');
+ });
+ app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+  });
 const {
 	NoSubscriberBehavior,
 	StreamType,
